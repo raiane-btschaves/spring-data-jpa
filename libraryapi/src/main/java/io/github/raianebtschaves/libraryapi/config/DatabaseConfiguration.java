1a27c8f7d4ba5b9335eae6d97a0cc4dea30d9009
RaianeBatista <202311169804@alunos.estacio.br>
2026-05-06 09:24:42 -0300

    feat(db): configurar pool de conexões HikariCP, add anotações de estudo

---
+++ libraryapi/src/main/java/io/github/raianebtschaves/libraryapi/config/DatabaseConfiguration.java
@@ -0,0 +1,59 @@
+package io.github.raianebtschaves.libraryapi.config;
+
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfiguration {
+
+    @Value("${spring.datasource.url}")
+    String url;
+    @Value("${spring.datasource.username}")
+    String username;
+    @Value("${spring.datasource.password}")
+    String password;
+    @Value("${spring.datasource.driver-class-name}")
+    String driver;
+
+  //  @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        ds.setDriverClassName(driver);
+        return ds;
+    }
+
+    /*
+    * Documentação HIKARI: https://github.com/brettwooldridge/hikaricp
+    * */
+
+
+
+    @Bean
+    public DataSource hikariDataSource() {
+
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driver);
+
+        config.setMaximumPoolSize(10); // ,aximo de conexões liberadas
+        config.setMinimumIdle(1); // tamanho iniical do pool
+        config.setPoolName("library-db-poll");
+        config.setMaxLifetime(600000); //600 mil milisegundos
+        config.setConnectionTimeout(100000); // timeout para conseguir uma conexão
+        config.setConnectionTestQuery("select 1"); // query de teste
+
+        return new HikariDataSource(config);
+    }
+}
